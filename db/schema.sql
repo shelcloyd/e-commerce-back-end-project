c34f978efadb92056579049339df48f1ccd187dd
Shel Cloyd <shel.cloyd@gmail.com>
2022-12-27 09:44:04 -0800

    added schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,21 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+CREATE TABLE category (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE product (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(50) NOT NULL,
+    price DECIMAL NOT NULL,
+    stock INT NOT NULL,
+    category_id INT
+);
+
+CREATE TABLE tag (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_id INT,
+    tag_id INT
+);
