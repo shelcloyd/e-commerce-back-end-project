754896bfc1d8375fdbe83e4bc00a587c4debf4c7
Shel Cloyd <shel.cloyd@gmail.com>
2022-12-27 10:16:18 -0800

    set up relationships between category, products, and tags

--- models/index.js
+++ models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  primaryKey: 'id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  primaryKey: 'id'
+});
 
 module.exports = {
   Product,
